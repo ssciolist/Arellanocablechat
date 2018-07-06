0436d8d6ce72b06d0ecd25208097836bdd5c9a41
Megan Arellano <ssciolist@gmail.com>
2018-07-06 10:55:15 -0600

    adds action cable meta tag to application.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_cable.url = 'ws://localhost:3000/cable'
+  config.action_cable.allowed_request_origins = [ 'http://localhost:3000', 'http://127.0.0.1:3000' ]
 
   # Do not eager load code on boot.
   config.eager_load = false
