10477eeb18037ffd6241589106a490f8e41121e0
Megan Arellano <ssciolist@gmail.com>
2018-07-06 10:53:02 -0600

    mounts cable, adds redis and some other javascript assets that may not be used

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/chat_room/:id", to: 'chat_room#show'
